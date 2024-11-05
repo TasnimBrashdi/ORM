6c5b65cabb0c9a0822a0f9d74c05bf3db87d2a36
Tasnim <tasnimalbrashdi@gmail.com>
2024-11-05 15:02:34 +0400

    add Dlocation , Project class

---
+++ ORM/Models/Dlocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Models
+{
+    public class Dlocation
+    {
+        [ForeignKey("Department")]
+        public int Dnumber { get; set; }
+        public virtual Department Department { get; set; }
+
+
+        public string Deplocation { get; set; }
+    }
+}
---
+++ ORM/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Pnumber { get; set; }
+
+        [Required]
+        public string Pname { get; set; }
+        public string Plocation { get; set; }
+
+        [ForeignKey("Department")]
+        public int Dnum { get; set; }
+
+        public virtual Department Department { get; set; }
+
+
+
+    }
+}
