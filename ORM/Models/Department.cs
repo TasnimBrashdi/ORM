bde163bed1a3713a1448098d798104a4bfa0a49d
Tasnim <tasnimalbrashdi@gmail.com>
2024-11-05 14:55:19 +0400

    add Department, Employee class

---
+++ ORM/Models/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Dnumber { get; set; }
+        public string DName { get; set; }
+
+        [ForeignKey("Manager")]
+        public int Mgrssn { get; set; }
+
+        public virtual Employee Manager { get; set; }
+
+        public DateTime MStartDate { get; set; }
+
+
+
+    }
+}
---
+++ ORM/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Ssn { get; set; }
+        [Required]
+        public string FName { get; set; }
+        [Required]
+        public string LName { get; set; }
+
+        public string Minit { get; set; }
+
+        public DateTime Bdate { get; set; }
+
+        public string Sex { get; set; } 
+        public decimal Salary { get; set; }
+
+         public string Address { get; set; }
+
+        [ForeignKey("Supervisor")]
+        public int? SuperSsn { get; set; }
+        public virtual Employee Supervisor { get; set; }
+
+        [InverseProperty("Supervisor")]
+        public virtual ICollection<Employee> Supervisees { get; set; }
+
+    }
+}
