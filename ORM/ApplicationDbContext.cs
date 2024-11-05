86d56b891397a9a11de5c86ef2398d3a0fc12c52
Tasnim <tasnimalbrashdi@gmail.com>
2024-11-05 14:20:11 +0400

    add ApplicationDbContext class

---
+++ ORM/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            options.UseSqlServer(" Data Source=(local); Initial Catalog=CompanyORM; Integrated Security=true; TrustServerCertificate=True ");
+        }
+
+    }
+}
